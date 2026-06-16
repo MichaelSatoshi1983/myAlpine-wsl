e0d334a00cdbad2eed025a069082ae2a465b67e2
Michael Mason <123161972+MichaelSatoshi1983@users.noreply.github.com>
2026-06-15 23:06:23 -0700

    Refactor build script for improved directory handling

--- scripts/build.sh
+++ scripts/build.sh
@@ -20,25 +20,41 @@ echo "download alpine rootfs"
 wget -qO alpine.tar.gz \
 https://dl-cdn.alpinelinux.org/alpine/v${MAJOR_VERSION}/releases/x86_64/alpine-minirootfs-${LATEST_VERSION}-x86_64.tar.gz
 
+echo "prepare rootfs"
+
 rm -rf "$ROOTFS"
-mkdir "$ROOTFS"
+mkdir -p "$ROOTFS"
 
 tar -xzf alpine.tar.gz -C "$ROOTFS"
 
 echo "prepare dns"
-cp /etc/resolv.conf "$ROOTFS/etc/"
+
+cp /etc/resolv.conf "$ROOTFS/etc/resolv.conf"
 
 echo "copy setup script"
+
 cp scripts/setup.sh "$ROOTFS/setup.sh"
 
 chmod +x "$ROOTFS/setup.sh"
 
 echo "enter chroot"
+
+mount --bind /dev "$ROOTFS/dev"
+mount --bind /proc "$ROOTFS/proc"
+mount --bind /sys "$ROOTFS/sys"
+
 chroot "$ROOTFS" /bin/sh /setup.sh
 
-echo "packaging"
-cd "$ROOTFS"
+echo "cleanup mounts"
 
-tar --numeric-owner -czf ../alpine-wsl.tar.gz .
+umount "$ROOTFS/dev"
+umount "$ROOTFS/proc"
+umount "$ROOTFS/sys"
+
+rm -f "$ROOTFS/setup.sh"
+
+echo "packaging"
+
+tar --numeric-owner -czf alpine-wsl.tar.gz -C "$ROOTFS" .
 
 echo "done"
