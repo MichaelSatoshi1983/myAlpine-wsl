1aa90220d43faeb12e2004763b481c768da4d096
Michael Mason <123161972+MichaelSatoshi1983@users.noreply.github.com>
2026-06-15 23:09:46 -0700

    Update build script for fetching Alpine version

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,22 +3,26 @@ set -e
 
 ROOTFS="rootfs"
 
-echo "fetch latest alpine version"
+echo "fetch latest alpine release metadata"
 
-LATEST_VERSION=$(wget -qO- \
+LATEST_FILE=$(wget -qO- \
 https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/latest-releases.yaml \
-| grep minirootfs \
+| grep "file: alpine-minirootfs" \
 | head -n1 \
-| awk '{print $2}')
+| cut -d' ' -f2)
+
+LATEST_VERSION=$(echo "$LATEST_FILE" \
+| sed -E 's/alpine-minirootfs-(.*)-x86_64.tar.gz/\1/')
 
 MAJOR_VERSION=$(echo "$LATEST_VERSION" | cut -d. -f1,2)
 
 echo "latest alpine version: $LATEST_VERSION"
 
+DOWNLOAD_URL="https://dl-cdn.alpinelinux.org/alpine/v${MAJOR_VERSION}/releases/x86_64/${LATEST_FILE}"
+
 echo "download alpine rootfs"
 
-wget -qO alpine.tar.gz \
-https://dl-cdn.alpinelinux.org/alpine/v${MAJOR_VERSION}/releases/x86_64/alpine-minirootfs-${LATEST_VERSION}-x86_64.tar.gz
+wget -qO alpine.tar.gz "$DOWNLOAD_URL"
 
 echo "prepare rootfs"
 
@@ -37,11 +41,13 @@ cp scripts/setup.sh "$ROOTFS/setup.sh"
 
 chmod +x "$ROOTFS/setup.sh"
 
-echo "enter chroot"
+echo "mount virtual filesystems"
 
 mount --bind /dev "$ROOTFS/dev"
 mount --bind /proc "$ROOTFS/proc"
 mount --bind /sys "$ROOTFS/sys"
+
+echo "enter chroot"
 
 chroot "$ROOTFS" /bin/sh /setup.sh
 
@@ -53,7 +59,7 @@ umount "$ROOTFS/sys"
 
 rm -f "$ROOTFS/setup.sh"
 
-echo "packaging"
+echo "packaging rootfs"
 
 tar --numeric-owner -czf alpine-wsl.tar.gz -C "$ROOTFS" .
 
